af0e0e9f62c9d7a26a5f86be4cbd77fe89be99af
zambonil <lucaszam0@gmail.com>
2021-02-06 13:02:01 -0300

    .

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   env: {
     SERVER_URL: "https://damp-wave-88141.herokuapp.com/ ",
-    S3_BUCKET_URL: "https://nextjszat.s3-sa-east-1.amazonaws.com/"
+    S3_BUCKET_URL: "https://nextjszat.s3-sa-east-1.amazonaws.com/",
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    S3_SECRET: process.env.S3_SECRET,
+    S3_REGION: process.env.S3_REGION,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
   },
 };
--- frontend/pages/api/upload-url.js
+++ frontend/pages/api/upload-url.js
@@ -3,23 +3,22 @@ import aws from "aws-sdk";
 export default async function handler(req, res) {
   console.log(req);
   aws.config.update({
-    accessKeyId: "AKIAI5EGKQZMS2RRCOHA",
-    secretAccessKey: "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",
-    region: "sa-east-1",
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET,
+    region: process.env.S3_REGION,
     signatureVersion: "v4",
   });
 
   const s3 = new aws.S3();
   const post = await s3.createPresignedPost({
-    Bucket: "nextjszat",
+    Bucket: process.env.S3_BUCKET_NAME,
     Fields: {
-      key: req.query.file,
+      key: req.body.formData.file,
     },
     Expires: 60, // seconds
     Conditions: [
       ["content-length-range", 0, 1048576], // up to 1 MB
     ],
   });
-  console.log(post);
   res.status(200).json(post);
 }
